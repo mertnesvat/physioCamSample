d6e00f1bfe5f207ced7801797004c66318f751c2
mert <mertnesvat@gmail.com>
2018-07-31 12:22:54 +0300

    initial commit

---
+++ cameraSample/CameraHandler.swift
@@ -0,0 +1,13 @@
+//
+//  CameraHandler.swift
+//  cameraSample
+//
+//  Created by Mert Neşvat on 7/31/18.
+//  Copyright © 2018 Mert Nesvat. All rights reserved.
+//
+import UIKit
+import Foundation
+
+func camera(){
+    
+}
