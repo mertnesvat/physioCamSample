e17871ef830d2003dbc97394ea3359578493b283
mert <mertnesvat@gmail.com>
2018-07-31 11:51:22 +0300

    Initial Commit

---
+++ cameraSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cameraSample
+//
+//  Created by Mert Neşvat on 7/31/18.
+//  Copyright © 2018 Mert Nesvat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
